59e1c0df70811a2f31607e38feb7f2ea2ddbeecc
Ahmad Fauzidan <zidankhainur2@gmail.com>
2025-01-24 16:10:58 +0700

    add dummy data

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,16 @@
-const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+import {
+  MdAdminPanelSettings,
+  MdKeyboardArrowDown,
+  MdKeyboardArrowUp,
+  MdKeyboardDoubleArrowUp,
+} from "react-icons/md";
+import { LuClipboardEdit } from "react-icons/lu";
+import { FaNewspaper, FaUsers } from "react-icons/fa";
+import { FaArrowsToDot } from "react-icons/fa6";
+import moment from "moment";
 
-export default Dashboard
+const Dashboard = () => {
+  return <div>Dashboard</div>;
+};
+
+export default Dashboard;
