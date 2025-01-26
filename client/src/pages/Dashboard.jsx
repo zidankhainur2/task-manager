026a5af29c5ba7d905d0fd9af0dfdbc4bdac8c30
Ahmad Fauzidan <zidankhainur2@gmail.com>
2025-01-26 16:00:36 +0700

    add dashboard v1

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,13 +4,65 @@ import {
   MdKeyboardArrowUp,
   MdKeyboardDoubleArrowUp,
 } from "react-icons/md";
-import { LuClipboardEdit } from "react-icons/lu";
+import { LuClipboard } from "react-icons/lu";
 import { FaNewspaper, FaUsers } from "react-icons/fa";
 import { FaArrowsToDot } from "react-icons/fa6";
 import moment from "moment";
+import { summary } from "../assets/data";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const totals = summary.tasks;
+
+  const stats = [
+    {
+      _id: "1",
+      label: "TOTAL TASK",
+      total: summary?.totalTasks || 0,
+      icon: <FaNewspaper />,
+      bg: "bg-[#1d4ed8]",
+    },
+    {
+      _id: "2",
+      label: "COMPLTED TASK",
+      total: totals["completed"] || 0,
+      icon: <MdAdminPanelSettings />,
+      bg: "bg-[#0f766e]",
+    },
+    {
+      _id: "3",
+      label: "TASK IN PROGRESS ",
+      total: totals["in progress"] || 0,
+      icon: <LuClipboard />,
+      bg: "bg-[#f59e0b]",
+    },
+    {
+      _id: "4",
+      label: "TODOS",
+      total: totals["todo"],
+      icon: <FaArrowsToDot />,
+      bg: "bg-[#be185d]" || 0,
+    },
+  ];
+
+  const Card = ({ label, count, bg, icon }) => {
+    return (
+      <div className="w-full h-32 bg-white p-3 shadow-md rounded-md flex items-center justify-between">
+        <div className="h-full flex flex-1 flex-col justify-between">
+          <p className="text-base text-gray-600">{label}</p>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="h-full py-4">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-5">
+        {stats.map(({ icon, bg, label, total }, index) => (
+          <Card key={index} icon={icon} bg={bg} label={label} count={total} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
