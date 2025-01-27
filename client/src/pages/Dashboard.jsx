41d69470d37844e446260a39179f3b278855e749
Ahmad Fauzidan <zidankhainur2@gmail.com>
2025-01-27 10:03:18 +0700

    add dashboard v2

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -9,6 +9,7 @@ import { FaNewspaper, FaUsers } from "react-icons/fa";
 import { FaArrowsToDot } from "react-icons/fa6";
 import moment from "moment";
 import { summary } from "../assets/data";
+import clsx from "clsx";
 
 const Dashboard = () => {
   const totals = summary.tasks;
@@ -49,6 +50,17 @@ const Dashboard = () => {
       <div className="w-full h-32 bg-white p-3 shadow-md rounded-md flex items-center justify-between">
         <div className="h-full flex flex-1 flex-col justify-between">
           <p className="text-base text-gray-600">{label}</p>
+          <span className="text-2xl font-semibold">{count}</span>
+          <span className="text-sm text-gray-400">{"110 last month"}</span>
+        </div>
+
+        <div
+          className={clsx(
+            "w-10 h-10 rounded-full flex items-center justify-center text-white",
+            bg
+          )}
+        >
+          {icon}
         </div>
       </div>
     );
